944e782963ad8a87ec06c7ff4d68b9e35f6fcb98
bashirovabduaziz <bashirovabduaziz66@gmail.com>
2023-12-07 19:39:57 +0500

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-[#0F0F0F] h-[300px] mt-[100px]'>
+    <div className='container '>
+         <div className="md:flex items-center justify-between sm:flex-row mt-8">
+          <ul className='flex gap-[140px] mt-[50px]'>
+            <li className='cursor-pointer '>
+              <p className='font-[400] text-white '>Home</p> 
+              <p className='text-[#999999] text-[200] mt-[10px]'>Categories</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Devices</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Pricing</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>FAQ</p>
+            </li>
+            <li className='cursor-pointer'>
+              <p className='font-[400] text-white '>Movies</p> 
+              <p className='text-[#999999] text-[200] mt-[10px]'>Gernes</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Trending</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>New Release</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Popular</p>
+            </li>
+            <li className='cursor-pointer'>
+              <p className='font-[400] text-white '>Shows</p> 
+              <p className='text-[#999999] text-[200] mt-[10px]'>Gernes</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Trending</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>New Release</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Popular</p>
+            </li>
+            <li className='cursor-pointer'>
+              <p className='font-[400] text-white'>Support</p>
+              <p className='text-[#999999] text-[200] mt-[10px]'>Contact Us</p>
+            </li>
+            <li className='cursor-pointer'>
+            <p className='font-[400] text-white'>Subscription</p>
+              <p className='text-[#999999] text-[200] mt-[10px]'>Plans</p>
+              <p className='text-[#999999] text-[200] mt-[5px]'>Features</p>
+            </li>
+            <li className='cursor-pointer'>
+              <p className='font-[400] text-white'>Connect With Us</p>
+               <div className="flex gap-4 mt-4">
+                <a href="#" className='bg-[#1A1A1A] p-[10px] rounded-[6px]'>
+                <FaFacebook className='text-white'/>
+                </a>
+               <a href="#" className='bg-[#1A1A1A] p-[10px] rounded-[6px]'>
+               <FaTwitter className='text-white'/>
+               </a>
+               <a href="#" className='bg-[#1A1A1A] p-[10px] rounded-[6px]'>
+               <FaLinkedin className='text-white'/>
+               </a>
+               </div>
+            </li>
+          </ul>
+         </div>
+         <hr className='mt-[20px] border-[#262626]'/>
+         <div className='flex justify-between items-center mt-[25px]'>
+          <p className='text-[#999999]'>@2023 streamvib, All Rights Reserved</p>
+          <ul className='flex gap-5'>
+            <li className='text-[#999999]'>Terms of Use</li>
+            <li className='text-[#999999]'>Privacy Policy</li>
+            <li className='text-[#999999]'>Cookie Policy</li>
+          </ul>
+         </div>
+    </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import { FaPlay } from "react-icons/fa";
+
 
 const Home = () => {
   return (
     <div className='bg'>
         <Header />
+           <div className='container mt-[560px]'>
+                 <div className='className="md:flex items-center justify-between sm:flex-row mt-8"'>
+                  <h1 className='text-white font-[700] text-[46px] text-center'>The Best Streaming Experience</h1>
+                  <p className='text-[18px] text-[#999999] text-center font-[400] mt-6'>StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. You can also create your own watchlists, so you can easily find the content you want to watch.</p>
+                 </div>
+                 <button className='text-white flex items-center gap-2 font-[600] bg-[#E50000] p-[15px] mt-6 border border-[#E50000] rounded-[6px] hover:text-[#E50000] hover:bg-[#141414] ml-[530px]'>
+                    <FaPlay />
+                    Start Watching Now
+                 </button>
+           </div>
+        <Footer />
     </div>
   )
 }
